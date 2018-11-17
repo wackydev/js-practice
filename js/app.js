77813726f0a7566005ff806b33a81b19fec61f1c
Arsalan Fazil (Wacky Dev) <wackydev@gmail.com>
2018-11-17 20:48:16 +0500

    resolve typo issue

--- js/app.js
+++ js/app.js
@@ -2,13 +2,18 @@ const Phone = 100;
 const Accessories = 50;
 const Tax = 4.40;
 const Spending = 500;
+var name = prompt('Please! Enter your name');
 var bankBalance = prompt('Please! Enter your bank balance');
 var amount = 0;
+var phonePurchased = 0;
+var accPurchased = 0;
 
 while(amount < bankBalance) {
   amount += Phone;
+	phonePurchased += 1;
   if(amount < Spending) {
     amount += Accessories;
+		accPurchased += 1;
   }
 }
 
@@ -18,6 +23,11 @@ function calculateTax(amount) {
 function formatAmount(amount) {
   return "$" + amount;
 }
+function capitalize(name) {
+	var firstLetter = name.charAt(0).toUpperCase();
+	var nameSlice = name.slice(1);
+	return firstLetter + nameSlice;
+}
 
 amount = calculateTax(amount);
 
@@ -26,4 +36,8 @@ if(amount < bankBalance) {
 } else {
   console.log("Sorry! you can't afford");
 }
-console.log(formatAmount(amount));
+console.log('Mr/Mrs ' + capitalize(name));
+console.log('Total Phone Purchased: ' + phonePurchased);
+console.log('Total Accessories Purchased: ' + accPurchased);
+console.log('Your Bank Balance:' + formatAmount(bankBalance));
+console.log("Total Purchased Amount: " + formatAmount(amount));
